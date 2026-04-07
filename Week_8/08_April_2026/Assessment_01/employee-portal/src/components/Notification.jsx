dc3909db084d13e6ed73e48d201a1508f38c92a4
kashhishh <kashishrastogii04@gmail.com>
2026-04-07 13:13:02 +0530

    del

--- Week_8/08_April_2026/Assessment_01/employee-portal/src/components/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import { useUI } from "../contexts/UIContext";
-
-export default function Notification() {
-  const { notification } = useUI();
-  if (!notification) return null;
-  return <div className={`notification ${notification.type}`}>{notification.message}</div>;
-}
